f31fd696640319ce956592d8022426aa4bc57d9b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-03-28 11:44:48 +0500

    Update Bar Plot of Stock Correlations.R
    Rewrote script for data extraction so redomiciliated stocks can be retrieved even if they have different time series before and after redomiciliation

--- Scripts for Plots/Bar Plot of Stock Correlations.R
+++ Scripts for Plots/Bar Plot of Stock Correlations.R
@@ -4,32 +4,74 @@ bar.plt.cor <- function(x, main=NULL, method="spearman"){
   
   p <- NULL
   
-  for (n in 1:length(x)){ # Get data of Russian stocks
-    
-    D = as.data.frame(get_candles(x[n],"2007-01-01",interval='daily')[,c(3,8)])
-    
-    D <- D[!duplicated(D),] # Remove duplicates
-    
-    D <- xts(D[,1], order.by = as.Date(D[,2])) # Move dates to row names
-    
-    D <- D[apply(D, 1, function(x) all(!is.na(x))),] # Get rid of NA
-    
-    colnames(D) <- x[n] # Put the tickers in data set
-    
-    D <- as.timeSeries(D) # Make it time series
-    
-    if (x[n] == "BELU"){ f <- which(rownames(D) == "2024-08-15")
-    
-    D[c(1:f),] <- D[c(1:f),]/8 } # Adjustments for Novabev stock
-    
-    message(
-      sprintf(
-        "%s is downloaded; %s from %s", 
-        x[n], which(x == x[n]), length(x)
-      )
-    )
+  redom = list(
+    c("AGRO", "RAGR"), c("CIAN", "CNRU"), c("HHRU", "HEAD"), c("FIVE", "X5"),
+    c("FIXP", "FIXR"), c("YNDX", "YDEX"))
   
-    p <- cbind(p, D) }
+  from = "2007-01-01"
+  
+  R <- NULL
+  
+  for (n in 1:length(x)){
+    
+    if (any(sapply(redom, function(redom_item) x[n] %in% redom_item))){
+      
+      f <- which(sapply(redom, function(redom_item) x[n] %in% redom_item))
+      
+      for (k in 1:length(redom[[f]])){
+        
+        a = as.data.frame(
+          get_candles(redom[[f]][k], from=from, interval='daily')[,c(3,8)]
+        )
+        
+        if (k == 2){ 
+          
+          message(
+            sprintf(
+              "%s is downloaded; %s from %s", x[n], which(x == x[n]), length(x)
+            )
+          )
+        }
+        
+        a <- a[!duplicated(a),] # Remove duplicates
+        
+        a <- xts(a[, 1], order.by = as.Date(a[, 2]))
+        
+        if (x[n] == "AGRO") a <- a / 7.01
+        
+        colnames(a) <- redom[[f]][2]
+        
+        if (is.null(R)) R <- data.frame(a) else R <- rbind.data.frame(R, a)
+      }
+    } else {
+      
+      a = as.data.frame(get_candles(x[n], from=from, interval='daily')[,c(3,8)])
+      
+      message(
+        sprintf(
+          "%s is downloaded; %s from %s", 
+          x[n], which(x == x[n]), length(x)
+        )
+      )
+      
+      a <- a[!duplicated(a),] # Remove duplicates
+      
+      a <- xts(a[, 1], order.by = as.Date(a[, 2]))
+      
+      colnames(a) <- x[n]
+      
+      R <- data.frame(a) 
+    }
+    
+    R <- as.timeSeries(R) # Make it time series
+    
+    if (x[n] == "BELU"){ j <- which(rownames(R) == "2024-08-15")
+    
+      R[c(1:j),] <- R[c(1:j),]/8 } # Adjustments for Novabev stock
+    
+    p <- cbind(p, R) 
+    R <- NULL  # Reset R for next iteration
+  }
   
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Eliminate NAs
   
